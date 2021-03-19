fed6ff709c167ed6751f5223f56d28ed262bba48
Xot64 <hottaby4hrip@mail.ru>
2021-03-19 17:22:43 +0300

    InitialClasses
    Создание первого дерева классов (3 уровня).
    Тестирование смены материала бонуса.

---
+++ Assets/Scripts/C1_Actor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class C1_Actor : MonoBehaviour
+{
+    public float X, Y;
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/C3_Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C3_Player : C2_Tank
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+
+    }
+}
